7ef07069da42adce96129a149f9f9c36ed9bd46d
yunhai <haihai107@126.com>
2021-08-02 00:13:51 +0800

    common shell scripts for better plantable

--- bin/epoll_start.sh
+++ bin/epoll_start.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 # /bin/sh -> /bin/dash, do not recognize our for loop
 
-for((i=0;i<10;i=i+1))
+i=0
+while [ $i -lt 10 ]; 
 do
     ./epoll_clt 1025 < demo &
     echo "start "
+    i=$(($i+1))
 done
